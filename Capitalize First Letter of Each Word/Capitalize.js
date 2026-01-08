bdee753a01cf405f7d6d788279adf3f816d4d905
Sumi Akter <sumiwdcse@gmail.com>
2026-01-08 13:13:28 +0600

    solve problem 8

---
+++ Capitalize First Letter of Each Word/Capitalize.js
@@ -0,0 +1,14 @@
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let word of words) {
+    let capitalized =
+      word[0].toUpperCase() + word.slice(1);
+    result.push(capitalized);
+  }
+
+  return result.join(" ");
+}
+
+console.log(capitalizeWords("hello world"));
